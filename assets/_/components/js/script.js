eb29e4e72c9e49b83e201ae6808efabfe8c18a3a
Thomas Dodson <dodson.t@gmail.com>
2014-09-02 08:56:02 -0400

    added smooth scrolling for anchor links

--- assets/_/components/js/script.js
+++ assets/_/components/js/script.js
@@ -1,5 +1,18 @@
-function test() {
-	return "hi there worlds"  
-}
+(function ($) {
+    $(document).ready(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+              || location.hostname == this.hostname) {
 
-var a = 6;;;
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                 if (target.length) {
+                   $('html,body').animate({
+                       scrollTop: target.offset().top
+                  }, 1000);
+                  return false;
+              }
+          }
+      });
+  }); //end document ready function
+})(jQuery);
